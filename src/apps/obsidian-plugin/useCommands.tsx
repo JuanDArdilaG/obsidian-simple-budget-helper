f2bc09f4674ece8d1a4547f5bdf447264fef6dd7
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2025-09-21 16:35:46 -0500

    refactor: remove unused PerCategoryRightSidebarReactTab component and clean up useCommands

--- src/apps/obsidian-plugin/useCommands.tsx
+++ src/apps/obsidian-plugin/useCommands.tsx
@@ -4,20 +4,4 @@ export const useCommands = ({
 	plugin,
 }: {
 	plugin: SimpleBudgetHelperPlugin;
-}) => {
-	// useEffect(() => {
-	// 	plugin.addCommand({
-	// 		id: "display-create-budget-item-modal",
-	// 		name: "Create budget item",
-	// 		callback: async () => {
-	// 			new CreateBudgetItemModalRoot(
-	// 				plugin.app,
-	// 				budget,
-	// 				async (item) => {
-	// 					await updateFiles(item, "add");
-	// 				}
-	// 			).open();
-	// 		},
-	// 	});
-	// }, [plugin, budget, updateFiles]);
-};
+}) => {};
